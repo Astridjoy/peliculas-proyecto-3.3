783812ffb8edd7cef708a941f2203bc8251fe28d
Astridjoy <168805080+Astridjoy@users.noreply.github.com>
2026-05-14 20:03:53 -0600

    Prueba ruta principal

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
 Route::get('/', function () {
-    return redirect('/movies');
+    return "Laravel funcionando 😄";
 });
 
 Route::get('/movies', [MovieController::class, 'index']);
